77a7f103bce70c70d0be48ea6aad59db569c5786
janinquisitor <jancarlos0195@gmail.com>
2022-02-28 19:59:05 -0400

    Added README.md file

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# password-gen_module
+# Password-gen_script
+
+## Description
+
+This is a safe password generator made with JavaScript. There are multiple options where you can select from to generate a secure password that suits you. From 8 up to 128 characters, you can make a completely safe and indecipherable password.
+
+
+## Usage
+
+Click on the **generate password** button and then fill up the prompts to specify what the kind of password want and thats it!
+
+![Password generator websute](./assets/img/Screen Shot 2022-02-28 at 7.52.37 PM.png)
+
+## Installation
+
+You can clone the project from the this [repository!](https://github.com/JanInquisitor/password-gen_script)
+
+
+```
+git clone https://github.com/JanInquisitor/password-gen_script
+
+```
+
+## Contributing
+
+Anyone is welcome to contribute to this project and implement better refactoring practices
+
+## License
+
+See the [LICENSE](https://www.google.com/) file for information.
--- script.js
+++ script.js
@@ -12,10 +12,10 @@ var specialChars = "~!@#$%^&*,-.:;()_+{}[]<=>";
 
 function generatePassword(properties) {
   // create empty string
-  var newSet = characters;
+  var newSet = "";
   var newPassword = "";
 
-  // in case no options are selected for special characters or numbers then creates a only letters password
+  if (properties.lowerCase === true) newSet += characters;
   if (properties.upperCase === true) newSet += upperCase;
   if (properties.specialChars === true) newSet = newSet + specialChars;
   if (properties.numbers === true) newSet = newSet + numbers;
@@ -38,12 +38,13 @@ function getProperties() {
     "Pleaser enter the length of your password (between 8 to 128 characters)"
   );
 
-  // This stops the program if the user click 'cancel'.
+  // This stops the program if the user clicks 'cancel'.
   if (properties.length === null) return false;
 
+  // This convert any string number into a number
   properties.length = properties.length * 1;
 
-  if (typeof properties.length === "string" || properties.length === NaN) {
+  if (properties.length === NaN) {
     alert("Must enter a number");
     return false;
   }
@@ -52,6 +53,10 @@ function getProperties() {
     alert("The length is too short or too long (8-128)");
     return false;
   }
+
+  properties.lowerCase = confirm(
+    "Woukd you like the password to contain lowercase characters?"
+  );
 
   properties.upperCase = confirm(
     "Woukd you like the password to contain uppercase characters?"
@@ -64,6 +69,17 @@ function getProperties() {
   properties.numbers = confirm(
     "Would you like the password to contain numbers?"
   );
+
+  if (
+    !properties.lowerCase &&
+    !properties.upperCase &&
+    !properties.specialChars &&
+    !properties.numbers
+  ) {
+    alert("At least one character type must be selected.");
+    return false;
+  }
+
   return properties;
 }
 
